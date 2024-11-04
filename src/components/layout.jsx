daf2dcde10c53bf40cdafe5aa815c98dbea88522
Rodrigo Agustin Alfaro Olmos <rodrigo.alfaroo@usm.cl>
2024-11-04 20:12:59 -0300

    merge fixes

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import HomePage from '../pages/home_page'
-import LightbulbPage from '../pages/lightbulb_page'
 import ExpositoresPage from '../pages/expositores_page'
 import ContactoPage from '../pages/contacto_page'
 import AboutPage from '../pages/about_page'
@@ -20,13 +19,11 @@ const Layout = () => {
   return (
     <BrowserRouter>
       <div className='layout'>
-
+    <NavBar />
          <h1 className='layout__title'> </h1>
-        <NavBar />
         <div className='layout__page'>
           <Routes>
             <Route path='/' element={<HomePage />} />
-            <Route path='/lightbulb' element={<LightbulbPage />} />
             <Route path='/expositores' element={<ExpositoresPage />} />
             <Route path='/contacto' element={<ContactoPage />} />
             <Route path='/about' element={<AboutPage />} />
@@ -34,9 +31,6 @@ const Layout = () => {
             <Route path='/rrss' element={<RedesSocialesPage />} />
             <Route path='/equipo' element={<ProjectTeamPage />} />
             <Route path='/detalles' element={<ProjectDetallesPage />} />
-
-      <NavBar />
-
           </Routes>
         </div>
       </div>
